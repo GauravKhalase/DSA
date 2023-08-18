64526c4f21e41a900ef561e2dca0ba13c2225186
GauravKhalase <gauravkhalase@gmail.com>
2023-08-18 22:59:31 +0530

    updating pattern problems

--- 1_Patterns/tempCodeRunnerFile.cpp
+++
@@ -1,9 +0,0 @@
-for (int col = 0; col < row + 1; col++)
-        // {
-        //     cout << start;
-        //     start=start+1;
-        //     if (col != row)
-        //     {
-        //         cout << "*";
-        //     }
-        // }
