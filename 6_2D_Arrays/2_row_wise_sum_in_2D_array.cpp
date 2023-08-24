4e4521005e09af68d1f39b228cf2a08d0c61a8dd
GauravKhalase <gauravkhalase@gmail.com>
2023-08-24 10:44:23 +0530

    addind 2D array problems

---
+++ 6_2D_Arrays/2_row_wise_sum_in_2D_array.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void printRowWiswSum(int arr[][3], int rows, int cols)
+{
+
+    for (int i = 0; i < rows; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < cols; j++)
+        {
+            sum = sum + arr[i][j];
+        }
+        cout << sum << endl;
+    }
+}
+
+int main()
+{
+    int arr[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    int rows = 3;
+    int cols = 3;
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    printRowWiswSum(arr, rows, cols);
+    return 0;
+}
